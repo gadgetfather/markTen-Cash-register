2cbbdf2229d4d3d6c7ba71b36e2e96d5845abcce
gadgetfather <kamblea635@gmail.com>
2021-09-13 16:51:21 +0530

    completed code

---
+++ app.js
@@ -0,0 +1,42 @@
+const billAmount = document.querySelector("#bill-amount")
+const cashGiven = document.querySelector("#cash-given")
+const checkBtn = document.querySelector("#check-btn")
+const errorMsg= document.querySelector("#error-message")
+const noOfNotes = document.querySelectorAll(".number-of-notes")
+const availNotes=[2000,500,100,50,20,10,1]
+
+checkBtn.addEventListener("click",function validate(){
+    hideMessage();
+   if(billAmount.value>0){
+       if(cashGiven.value >= billAmount.value){
+           const amountToBeReturned = cashGiven.value-billAmount.value
+           calculateChange(amountToBeReturned)
+           }
+       else{
+           showError("Do you wanna wash plates?")
+       }
+
+   }
+   else{
+       showError("Invalid bill amount")
+   }
+})
+
+function hideMessage(){
+    errorMsg.style.display ="none"
+}
+
+function showError(message){
+    errorMsg.style.display="block";
+    errorMsg.textContent=message
+}
+
+function calculateChange(amountToBeReturned){
+    for(let i = 0; i<availNotes.length ; i++){
+        const numberOfNotes = Math.trunc(amountToBeReturned/availNotes[i]
+            );
+            amountToBeReturned = amountToBeReturned % availNotes[i]
+            noOfNotes[i].innerText= numberOfNotes
+    }
+     
+}
