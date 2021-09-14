7e77dd5d28fc67bad176cd07007afe175c8d8e3a
gadgetfather <kamblea635@gmail.com>
2021-09-14 15:15:40 +0530

    Update app.js
    fixed bug. added 50rs note

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cashGiven = document.querySelector("#cash-given")
 const checkBtn = document.querySelector("#check-btn")
 const errorMsg= document.querySelector("#error-message")
 const noOfNotes = document.querySelectorAll(".number-of-notes")
-const availNotes=[2000,500,100,50,20,10,1]
+const availNotes=[2000,500,100,50,20,10,5,1]
 
 
 
